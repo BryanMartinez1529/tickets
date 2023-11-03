afef0d5c9efec04aba93e5452b404a5c84c7e3eb
Bryan <bryan.martinez6523@utc.edu.ec>
2023-11-02 22:52:20 -0500

    Crud de Clientes

--- application/models/Cliente.php
+++ application/models/Cliente.php
@@ -1,5 +1,51 @@
 <?php
+  class Cliente extends CI_Model
+  {
 
+    function __construct()
+    {
+        parent :: __construct();
+    }
+
+    function insertar ($datos){
+        return $this->db->insert('cliente',$datos);
+    }
+
+    function obtenerCliente(){
+        $listadoCliente=$this->db->get('cliente');
+        if ($listadoCliente->num_rows()>0){
+            return $listadoCliente->result();
+        } else{
+            return false;
+        }
+    }
+
+    public function borrar ($id_cli){
+
+        $this->db->where("id_cli",$id_cli);
+        return $this->db->delete("cliente");
+   
+    }
+       
+       public function ObtenerporId($id_cli)
+       {
+         $this->db->where("id_cli",$id_cli);
+         $cliente=$this->db->get("cliente");
+         if ($cliente->num_rows()>0){
+           return $cliente->row();
+         }
+          return false;
+   
+       }
+       //funcion para actualizar un instructor
+       public function actualizar($id_cli,$datos)
+       {
+         $this->db->where("id_cli",$id_cli);
+         return $this->db->update("cliente",$datos);
+       }
+   
+
+  }
 
 
 
--- application/views/clientes/editar.php
+++ application/views/clientes/editar.php
@@ -1,41 +1,42 @@
 <br>
 <br>
 <br>
-<h1>Ingrese los datos del Cliente</h1>
+<h1>Editar los datos del Cliente</h1>
 
 
 <div class="row g-3">
                     <div class="col-sm-12 col-xl-12">
                         <div class="bg-secondary rounded h-100 p-4">
-                            <h6 class="mb-4">Basic Form</h6>
-                            <form method="post" action="<?php echo site_url("/clientes/guardar"); ?>">
+                            <h6 class="mb-4">Formulario</h6>
+                            <form method="post" action="<?php echo site_url("/clientes/procesoActualizar"); ?>">
+                              <input type="hidden" name="id_cli" id="id_cli" value="<?php echo $editarCliente->id_cli ?>" >
                               <div class="row">
                                 <div class="col-md-4">
                                     <label for="" class="form-label">Nombre</label>
-                                    <input type="text"  name="nombre_cli" class="form-control" placeholder="Ingrese su nombre" id="nombre_cli">
+                                    <input type="text"  name="nombre_cli" class="form-control"  id="nombre_cli" value="<?php echo $editarCliente->nombre_cli ?>">
                                 </div>
                                 <div class="col-md-4">
                                     <label class="form-label">Apellido</label>
-                                    <input type="text" name="apellido_cli" placeholder="Ingrese su apellido" class="form-control" id="apellido_cli">
+                                    <input type="text" name="apellido_cli" class="form-control" id="apellido_cli" value="<?php echo $editarCliente->apellido_cli ?>">
                                 </div>
                                 <div class="col-md-4">
                                         <label for="" class="form-label">Cedula</label>
-                                        <input type="number" name="cedula_cli" class="form-control" placeholder="Ingrese su cedula" id="cedula_Cli">
+                                        <input type="number" name="cedula_cli" class="form-control"  id="cedula_cli" value="<?php echo $editarCliente->cedula_cli ?>">
                                     </div>
                               </div> 
                               <div class="row">
                                     
                                     <div class="col-md-4">
                                         <label for="" class="form-label">Direccion</label>
-                                        <input type="text" name="direccion_cli" class="form-control" placeholder="Ingrese su direccion" id="direccion_cli">
+                                        <input type="text" name="direccion_cli" class="form-control"  id="direccion_cli" value="<?php echo $editarCliente->direccion_cli ?>">
                                     </div>
                                     <div class="col-md-4">
                                         <label for="" class="form-label">Correo</label>
-                                        <input type="text" name="correo_cli" class="form-control" placeholder="Ingrese su correo" id="correo_cli">
+                                        <input type="text" name="correo_cli" class="form-control"  id="correo_cli" value="<?php echo $editarCliente->correo_cli ?>">
                                     </div>
                                     <div class="col-md-4">
                                         <label for="" class="form-label">Teléfono</label>
-                                        <input type="number"  name="telefono_cli" class="form-control" placeholder="Ingrese su teléfono" id="telefono_cli">
+                                        <input type="number"  name="telefono_cli" class="form-control"  id="telefono_cli" value="<?php echo $editarCliente->telefono_cli ?>">
                                     </div>
                               </div>
                               <div class="row">
